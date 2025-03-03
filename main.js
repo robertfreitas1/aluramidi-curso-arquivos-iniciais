52c6e721c924b1cc2a79a353c8aeb5c326dd1954
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-03-03 14:25:01 -0300

    algumas alterações

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play(); 
     
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla'); // a constante lista recebe todos os  seletores da classe "tecla"
 
+
+let contador = 0; 
+
+
+
+
+
+
+
+
+// enquanto
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+
+}
